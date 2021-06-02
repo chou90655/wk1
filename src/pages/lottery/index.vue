f963adbaae89fee869bdc73887978a5d61c41cf0
hhsj <zhou90655@gmail.com>
2021-06-02 22:21:51 +0800

    211221

---
+++ src/pages/lottery/index.vue
@@ -0,0 +1,34 @@
+<template>
+	<view>
+    <iframe src="" class="iframe" frameborder="0"></iframe>
+	</view>
+</template>
+
+<script>
+	import api from '@/api/api.js'
+	import graceChecker from '../../common/graceChecker.js'
+	import {mapState, mapGetters, mapActions} from 'vuex'
+	export default {
+		computed: {
+			...mapGetters(['user', 'remUser']),
+		},
+		data() {
+			return {
+			}
+		},
+    computed: {
+      ...mapState(['token'])
+    },
+		methods: {
+      
+    }
+	}
+</script>
+
+<style lang="scss">
+.iframe {
+  width: 100%;
+  height: 100%;
+  background-color: #fff;
+}
+</style>
